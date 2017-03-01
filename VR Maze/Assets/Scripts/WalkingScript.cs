cd2a24530af1af71042259a87a143c6e2b0a5265
JStuve <stuve.josh@gmail.com>
2017-03-01 00:02:50 -0600

    Walking is now working. Objects detect a collision and no longer walk through the wall.

--- VR Maze/Assets/Scripts/WalkingScript.cs
+++ VR Maze/Assets/Scripts/WalkingScript.cs
@@ -17,8 +17,8 @@ public class WalkingScript : MonoBehaviour {
 		camViewRotX = cameraView.transform.eulerAngles.x; //Angel of the camer >0 is looking down <0 looking up
 
 
-
-		speed = (float) Math.Pow(0.00001, (double)1/camViewRotX); //Formula that increase the speed the more you look down
+		speed = 3; //Temp for 
+		//speed = (float) Math.Pow(0.00001, (double)1/camViewRotX); //Formula that increase the speed the more you look down
 
 		//Testing purposes: Alter the speed and downward angle ratio
 		//print ("cam: " + camViewRotX);
@@ -33,20 +33,10 @@ public class WalkingScript : MonoBehaviour {
 
 		if (isWalking) {
 			
-			Vector3 cameraDirection = new Vector3 (cameraView.transform.forward.x, 0, cameraView.transform.forward.z).normalized * 2 * Time.deltaTime; //2 = speed
+			Vector3 cameraDirection = new Vector3 (cameraView.transform.forward.x, 0, cameraView.transform.forward.z).normalized * speed * Time.deltaTime; //2 = speed
 			Quaternion cameraRotation = Quaternion.Euler (new Vector3 (0, -transform.rotation.eulerAngles.y, 0));
 			transform.Translate (cameraRotation * cameraDirection);
 
-		}
-		else{
-			
-		}
-	}
-
-	void OnCollisionEnter(Collision collision){
-		if (collision.gameObject.tag == "wall") {
-
-			print ("Hit a dang wall!");
 		}
 	}
 }
