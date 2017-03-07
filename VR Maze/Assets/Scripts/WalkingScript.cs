4a076dff4b29aa948be3b5764247d2607295bd8b
JStuve <stuve.josh@gmail.com>
2017-03-06 19:10:27 -0600

    Threshold movement now works. The lower you look the faster you will move. Look slightly upward to stop. Removed a conflicting View in Automaze Generator. Added a Y-Rotaion constraint on the character to prevent rotation movement if the player runs into the wall to fast.

--- VR Maze/Assets/Scripts/WalkingScript.cs
+++ VR Maze/Assets/Scripts/WalkingScript.cs
@@ -7,9 +7,8 @@ public class WalkingScript : MonoBehaviour {
 	public Rigidbody playerRigidbody;
 	public CapsuleCollider playerCollider;
 
-
-	private bool isWalking;
-	private float speed;
+	private float currentSpeed = 0;
+	private float tempSpeed = 0;
 	private float camViewRotX;	//The value of the camera when you look up or doww
 
 	void Update(){
@@ -17,26 +16,39 @@ public class WalkingScript : MonoBehaviour {
 		camViewRotX = cameraView.transform.eulerAngles.x; //Angel of the camer >0 is looking down <0 looking up
 
 
-		speed = 5; //Temp for 
-		//speed = (float) Math.Pow(0.00001, (double)1/camViewRotX); //Formula that increase the speed the more you look down
-
-		//Testing purposes: Alter the speed and downward angle ratio
-		//print ("cam: " + camViewRotX);
-		//print ("Speed: " + speed);
-
-		if (camViewRotX > 5 && camViewRotX < 90) {
-			isWalking = true;
-		}
-		else{
-			isWalking = false;
-		}
-
-		if (isWalking) {
-			
-			Vector3 cameraDirection = new Vector3 (cameraView.transform.forward.x, 0, cameraView.transform.forward.z).normalized * speed * Time.deltaTime; //2 = speed
-			Quaternion cameraRotation = Quaternion.Euler (new Vector3 (0, -transform.rotation.eulerAngles.y, 0));
-			transform.Translate (cameraRotation * cameraDirection);
+		if (camViewRotX > 8 && camViewRotX < 90) {
+			speedUp (camViewRotX);
 
 		}
+		if (camViewRotX < 345 && camViewRotX > 270){
+			slowDown ();
+		}
+		walk (currentSpeed);
+	}
+
+	public void walk(float speed){
+
+		Vector3 cameraDirection = new Vector3 (cameraView.transform.forward.x, 0, cameraView.transform.forward.z).normalized * speed * Time.deltaTime; //2 = speed
+		Quaternion cameraRotation = Quaternion.Euler (new Vector3 (0, -transform.rotation.eulerAngles.y, 0));
+		transform.Translate (cameraRotation * cameraDirection);
+	}
+
+	public void speedControl(){
+		
+	}
+	public void speedUp(float angle){
+		currentSpeed = (float)Math.Pow (camViewRotX, 2) / 540;
+		if (currentSpeed >= tempSpeed) {
+			if (currentSpeed < 5) {
+				currentSpeed = 5;
+			}
+			tempSpeed = currentSpeed;
+		} else {
+			currentSpeed = tempSpeed;
+		}
+	}
+	public void slowDown(){
+		tempSpeed = 0;
+		currentSpeed = 0;
 	}
 }
