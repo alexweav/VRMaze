ddc3b1e08955033c159111e8134a94ad53d83fb4
alexweav <alexander.weaver@ttu.edu>
2017-03-25 17:36:53 -0500

    Fixed typo

--- VR Maze/Assets/Scripts/MazeGenerate.cs
+++ VR Maze/Assets/Scripts/MazeGenerate.cs
@@ -6,13 +6,13 @@ namespace Assets.Scripts
 {
     public class MazeGenerate : MonoBehaviour
     {
-		public int hieght = 15;
+		public int height = 15;
 		public int width = 15;
         // Use this for initialization
         void Start()
         {
             //DrawHardcodedMaze();
-			DrawRandomMaze(hieght, width);
+			DrawRandomMaze(width, height);
         }
 
         // Update is called once per frame
