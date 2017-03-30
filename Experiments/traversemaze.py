80ebd10a5084116145dd217833fcc4989767f670
alexweav <alexander.weaver@ttu.edu>
2017-03-30 11:04:44 -0500

    Store gradient data

--- Experiments/traversemaze.py
+++ Experiments/traversemaze.py
@@ -9,22 +9,42 @@ num_games = 1
 steps_per_game = 100
 
 def main():
-    #Create a new 10x10 maze
+    #Construct the model
     action_dim = 4
     architecture = [102, 75, action_dim]
     net = NeuralNet(architecture)
+
     for game in range(num_games):
+        #Create a new maze and set up initial observation
         graph = Graph(10, 10)
         graph.connect_maze()
         current_node = (0, 0)
         exploration_buffer = np.zeros((10, 10))
         exploration_buffer[(0, 0)] = graph.get_connection_code(current_node)
         observation = create_observation(exploration_buffer, current_node)
+
+        #Data stored at every game step:
+        observations = [] #The observation at each game step
+        hidden_activations = [] #The hidden layer activations of the net
+        d_log_probs = [] #Derivative of loss function
+        rewards = [] #Reward gained
+        total_reward = 0.0
         for stepnum in range(steps_per_game):
-            probabilities, _ = net.eval(observation)
+            #Get action from network
+            probabilities, hidden_activation = net.eval(observation)
             choice = net.make_choice(probabilities)
+            choice_vector = np.zeros_like(probabilities)
+            choice_vector[(0, choice)] = 1
+            #Perform the action
             current_node, reward = step(graph, current_node, choice)
+            #Cache data
+            observations.append(observation)
+            hidden_activations.append(hidden_activation)
+            d_log_probs.append(choice_vector - probabilities)
+            rewards.append(reward)
+            #Set up next observation
             exploration_buffer[current_node] = graph.get_connection_code(current_node)
+            observation = create_observation(exploration_buffer, current_node)
             print(choice, reward)
             print(exploration_buffer)
             input()
