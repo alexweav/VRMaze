fcf75b141714f95babc581e2ba7f9291f9282eb2
alexweav <alexander.weaver@ttu.edu>
2017-03-30 10:35:02 -0500

    Neural agent traverses maze randomly

--- Experiments/traversemaze.py
+++ Experiments/traversemaze.py
@@ -5,6 +5,9 @@ import numpy as np
 from graph import *
 from neuralnet import *
 
+num_games = 1
+steps_per_game = 100
+
 def main():
     #Create a new 10x10 maze
     graph = Graph(10, 10)
@@ -16,10 +19,14 @@ def main():
     action_dim = 4
     architecture = [np.prod(observation.shape), 75, action_dim]
     net = NeuralNet(architecture)
-    probabilities, _ = net.eval(observation)
-    choice = net.make_choice(probabilities)
-    print(probabilities)
-    print(choice)
+    for stepnum in range(steps_per_game):
+        probabilities, _ = net.eval(observation)
+        choice = net.make_choice(probabilities)
+        current_node, reward = step(graph, current_node, choice)
+        exploration_buffer[current_node] = graph.get_connection_code(current_node)
+        print(choice, reward)
+        print(exploration_buffer)
+        input()
 
 #A single game observation is a pair (exploration buffer, current node)
 #Represented in a row vector of integers
@@ -29,7 +36,18 @@ def create_observation(exploration_buffer, current_node):
     node = np.array([[row, col]])
     return np.concatenate((expl, node), axis=1)
 
-
-
+#Performs an action in the game
+#Returns a pair (new_current_node, reward)
+def step(graph, current_node, choice):
+    target_node = current_node
+    row, col = current_node
+    if choice == 0: target_node = (row-1, col) #up
+    if choice == 1: target_node = (row+1, col) #down
+    if choice == 2: target_node = (row, col-1) #left
+    if choice == 3: target_node = (row, col+1) #right
+    if graph.connected(current_node, target_node):
+        return target_node, 1
+    else:
+        return current_node, -1
 
 main()
