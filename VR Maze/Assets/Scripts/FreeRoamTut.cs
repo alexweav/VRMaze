de7e214b6df1a782c949ed7a92d58d055ca4b45b
JStuve <stuve.josh@gmail.com>
2017-04-03 23:08:32 -0500

    Configuring the state of the tutorail and how to manage it. Both, 'HUDTextAnimation' and 'FreeRoamTut' are in design mode. Pushing to TutBuild for access on other machines.

---
+++ VR Maze/Assets/Scripts/FreeRoamTut.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+	public class FreeRoamTut: MonoBehaviour {
+
+		public Text promptText;
+		public Image promptBox;
+
+
+
+		private string[] msgState = {"Welcome to VRMaze!\nLook around using your VR headset!","Great!\nTo move forward, look down!","To stop, look up towards the sky.\nYou don't need to look all the way up.","Well done!\nNow, find your way out of this maze."};
+		private int msgIndex = 0;
+		private bool taskCompeleted = false;
+
+		void Start(){
+			displayPrompt (false);
+			StartCoroutine (prompt (3.0f));
+		}
+
+		void Update(){
+			if (taskCompeleted) {
+				msgIndex++;	
+			}
+		}
+			
+		/// <summary>
+		/// IEnumberator works in conjunction with StartCoroutine()
+		/// </summary>
+		/// <returns>Edits promptBox</returns>
+		IEnumerator prompt(float delay){
+			yield return new WaitForSeconds(delay);
+			displayPrompt (true);
+			for (int i = 0; i < (msgState[msgIndex].Length+1); i++)
+			{
+				promptText.text = msgState[msgIndex].Substring(0, i);
+				yield return new WaitForSeconds(.03f);
+			}
+		}
+
+		public void displayPrompt(bool status){
+			promptBox.enabled = status;
+			promptText.enabled = status;
+		}
+	}
+}
+
--- VR Maze/Assets/Scripts/HUDTextAnimation.cs
+++ VR Maze/Assets/Scripts/HUDTextAnimation.cs
@@ -3,57 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Assests.Sciprts
+namespace Assets.Scripts
 {
-	public class HUDTextAnimation : MonoBehaviour {
+	public class HUDTextAnimation{
 		
-		public Text textBox;
-		public Image promptBox;
-		private string currentMessage = "Welcome to VRMaze!\n Look around using your VR headset!";
-		private bool updatedMessage = false;
+		private bool visible = false; 
 
-		public void setMessage(string msg){
-			currentMessage = msg;
-			updatedMessage = true;
-		}
-
-		public void showPrompt(bool status){
-			promptBox.enabled = status;
-			textBox.enabled = status;
-
-		}
-
-		/// <summary>
-		/// IEnumberator works in conjunction with StartCoroutine()
-		/// </summary>
-		/// <returns>Edits promptBox</returns>
-		IEnumerator showPrompt(){
-			showPrompt (true);
-			for (int i = 0; i < (currentMessage.Length+1); i++)
-			{
-				textBox.text = currentMessage.Substring(0, i);
-				yield return new WaitForSeconds(.03f);
+		public bool promptVisible{
+			get{
+				return visible;
 			}
-		}
-
-		IEnumerator delayPrompt(){
-			yield return new WaitForSeconds (2.0f);
-			StartCoroutine(showPrompt ());
-		}
-			
-		/// <summary>
-		/// StartCoroutine() excellent for modelling behaviours over several frames.
-		/// </summary>
-		void Awake() {
-			showPrompt (false);
-			StartCoroutine(delayPrompt());
-
-		}
-
-		void Update(){
-			if (updatedMessage == true){
-				StartCoroutine(showPrompt());
-				updatedMessage = false;
+			set{
+				visible = value;
 			}
 		}
 	}
