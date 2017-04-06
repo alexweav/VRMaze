5334a16bf9521b4c3f74fe34accdcaed8c8343e8
alexweav <alexander.weaver@ttu.edu>
2017-04-06 11:53:20 -0500

    Tensorflow parameters

--- Experiments/traversemazeten.py
+++ Experiments/traversemazeten.py
@@ -3,14 +3,24 @@ import numpy as np
 import tensorflow as tf
 
 from graph import *
+from env_traversemaze import *
 
-num_games = 10000
-steps_per_game = 100
+num_games = 1
+steps_per_game = 10
 maze_size = 5
 print_every = 100
 
+H = 1000
+learning_rate = 1e-3
+gamma = 0.99
+
 def main():
-    action_dim = 4
-    architecture = [maze_size*maze_size+2, 500, action_dim]
+    env = TraverseMazeEnvironment(maze_size)
+    for game in range(num_games):
+        observation = env.reset()
+        for step in range(steps_per_game):
+            observation, reward = env.step(np.random.randint(0, 4))
+            print(observation)
+            print(reward)
 
 main()
