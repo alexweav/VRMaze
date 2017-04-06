0e6ca94346493e2200f01e24ddff719a51d97769
alexweav <alexander.weaver@ttu.edu>
2017-04-06 11:59:51 -0500

    Build tensorflow model

--- Experiments/traversemazeten.py
+++ Experiments/traversemazeten.py
@@ -23,4 +23,18 @@ def main():
             print(observation)
             print(reward)
 
+def build_model():
+    D = maze_size*maze_size
+    tf.reset_default_graph()
+    observations = tf.placeholder(tf.float32, [None, D], name="input_x")
+    W1 = tf.get_variable("W1", shape=[D, H], initializer=tf.conrib.layers.xavier_initializer())
+    layer1 = tf.nn.relu(tf.matmul(observations, W1))
+    W2 = tf.get_variable("W2", shape=[H, 1], initializer=tf.contrib.layers.xavier_initializer())
+    score = tf.matmul(layer1, W2)
+    probability = tf.nn.softmax(score)
+
+    tvars = tf.trainable_variables()
+    input_y = tf.placeholder(tf.float32, [None, 1], name="input_y")
+    advantages = tf.placeholder(tf.float32, name="reward_signal")
+
 main()
