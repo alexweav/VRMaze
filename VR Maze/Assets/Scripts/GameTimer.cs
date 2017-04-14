8c5a9d379f93154383b8fb4c7c14df0368c722ed
stedixon <Stephanie.dixon921@gmail.com>
2017-04-14 01:33:51 -0500

    Pause Enabled for GameTimer
    GameTimer script now enables pause. GVR Reticle in FreeRoam changed to
    white color from blue.

--- VR Maze/Assets/Scripts/GameTimer.cs
+++ VR Maze/Assets/Scripts/GameTimer.cs
@@ -7,31 +7,53 @@ public class GameTimer : MonoBehaviour
 {
 
     public float MaxGameTime = 600; //equivalent of 10 minutes
-    public Text timerText; //text element to display time in maze
     public Camera player; //camera -- allows timer to match rotation
-    public Canvas c; //canvas text is placed on, allows text to rotate with player
+
+    private Canvas c; //canvas text is placed on, allows text to rotate with player
+    private Text timerText; //text element to display time in maze
+
+    private bool paused = false;
     private float duration = 0; //total time in maze
+    private float pauseTime = 0; //time at pause
 
     // Use this for initialization
     void Start()
     {
         //find start time
         duration = Time.deltaTime;
+        c = gameObject.GetComponentInParent<Canvas>();
+        timerText = gameObject.GetComponentInParent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //find time in maze
-        duration += Time.deltaTime;
-        //update minutes and seconds
-        int minutes = Mathf.FloorToInt(duration / 60F);
-        int seconds = Mathf.FloorToInt(duration - minutes * 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (!paused)
+        {
+            //find time in maze
+            duration += Time.deltaTime;
+            //update minutes and seconds
+            int minutes = Mathf.FloorToInt(duration / 60F);
+            int seconds = Mathf.FloorToInt(duration - minutes * 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
 
         //update canvas rotation
         float rotation = player.transform.eulerAngles.y - c.transform.eulerAngles.y;
-
         c.transform.Rotate( 0, 0, rotation, Space.Self);
+    }
+
+    public void pauseTImer(bool pause)
+    {
+        if(pause)
+        {
+            pauseTime = duration;
+            paused = true;
+        }
+        else
+        {
+            duration = pauseTime - Time.deltaTime;
+            paused = false;
+        }
     }
 }
