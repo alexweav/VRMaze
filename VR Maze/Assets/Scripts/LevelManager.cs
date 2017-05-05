03604ed0ae111ea933f50c2be9aabd670fb88391
JStuve <stuve.josh@gmail.com>
2017-05-05 13:22:45 -0500

    Added new option menu for free roam.

--- VR Maze/Assets/Scripts/LevelManager.cs
+++ VR Maze/Assets/Scripts/LevelManager.cs
@@ -4,178 +4,197 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Assets.EditorTools;
 
-public class LevelManager : MonoBehaviour
-{
+namespace Assets.Scripts{
+	public class LevelManager : MonoBehaviour
+	{
 
-    public Transform mainMenu;
-    public Transform instructMenu;
-    public Transform optionMenu;
-    public Transform loading;
+	    public Transform mainMenu;
+	    public Transform instructMenu;
+	    public Transform optionMenu;
+	    public Transform loading;
+		public Transform freeRoamOptionMenu;
 
-    public float timer = 3f;//length of gaze required before action is taken
-    private float lookTimer = 0f;//length of time the user has looked at an object
-    private Renderer myRenderer;
-    private BoxCollider myCollider;
-    private bool isLookedAt = false;//set if an object is currently being looked at
+	    public float timer = 3f;//length of gaze required before action is taken
+	    private float lookTimer = 0f;//length of time the user has looked at an object
+	    private Renderer myRenderer;
+	    private BoxCollider myCollider;
+	    private bool isLookedAt = false;//set if an object is currently being looked at
 
-    [ReadOnly]
-    public bool isLoading = false;
+	    [ReadOnly]
+	    public bool isLoading = false;
 
-    /*********************************************************
-     variables are set when the corresponding button is pressed 
-     **********************************************************/
-    private bool startScene = false; //switches scenes
-    private bool showInstruct = false; //switches to InstructMenu
-    private bool goBack = false; //returns to previous menu
-    private bool showOptions = false; //switches to game options menu
+	    /*********************************************************
+	     variables are set when the corresponding button is pressed 
+	     **********************************************************/
+	    private bool startScene = false; //switches scenes
+	    private bool showInstruct = false; //switches to InstructMenu
+	    private bool goBack = false; //returns to previous menu
+	    private bool showOptions = false; //switches to game options menu
+		private bool showFreeRoamOptions = false; //switch to Free Roam Difficulty menu
 
-    private string scene = "MainMenu"; //initially set to load itself if no other scene is designated
+	    private string scene = "MainMenu"; //initially set to load itself if no other scene is designated
 
-    private void Start()
-    {
-        myCollider = GetComponent<BoxCollider>();
-        myRenderer = GetComponent<Renderer>();
-        myRenderer.material.SetFloat("cutoff", 0f);
-    }
-    /****************************************************************************************************
-     * Update()
-     * 
-     * This function checks continuously to determine if the user is looking at any interactive object
-     * on the screen, if the use looks at any one interactive item for at least the length required by 
-     * timer then an action is taken based on the object being looked at.
-     ***************************************************************************************************/
-    private void Update()
-    {
-        if (isLookedAt && !isLoading) //iff an item is being looked at by the user
-        {
-            lookTimer += Time.deltaTime; //get length of look
+	    private void Start()
+	    {
+	        myCollider = GetComponent<BoxCollider>();
+	        myRenderer = GetComponent<Renderer>();
+	        myRenderer.material.SetFloat("cutoff", 0f);
+	    }
+	    /****************************************************************************************************
+	     * Update()
+	     * 
+	     * This function checks continuously to determine if the user is looking at any interactive object
+	     * on the screen, if the use looks at any one interactive item for at least the length required by 
+	     * timer then an action is taken based on the object being looked at.
+	     ***************************************************************************************************/
+	    private void Update()
+	    {
+	        if (isLookedAt && !isLoading) //iff an item is being looked at by the user
+	        {
+	            lookTimer += Time.deltaTime; //get length of look
 
-            myRenderer.material.SetFloat("cutoff", lookTimer / timer);
+	            myRenderer.material.SetFloat("cutoff", lookTimer / timer);
 
-            if (lookTimer > timer) //if length is at the threshold length
-            {
-                //Debug.Log("Time "+lookTimer);
-                //lookTimer = 0f; //reset the lookTimer
-                myCollider.enabled = false;
-                /*************************************************************
-                 * depending on which action is to be taken, the proper
-                 * function will be called
-                 * **********************************************************/
-                if (startScene == true) //switch scenes
-                {
-                    loading.gameObject.SetActive(true);
-                    optionMenu.gameObject.SetActive(false);
-                    isLoading = true;
-                    StartCoroutine(LoadSceneAsync());
-                }
-                else if (showOptions == true) //switch to options menu
-                {
-                    //Debug.Log("options");
-                    optionMenu.gameObject.SetActive(true);
-                    mainMenu.gameObject.SetActive(false);
-                }
-                else if (showInstruct == true) //show instructions menu
-                {
-                    //Debug.Log("instructions");
-                    instructMenu.gameObject.SetActive(true);
-                    mainMenu.gameObject.SetActive(false);
-                }
-                else if (goBack == true) //return to previous menu
-                {
-                    //Debug.Log("back");
-                    if (instructMenu.gameObject.activeSelf)
-                    {
-                       // Debug.Log("inst");
-                        mainMenu.gameObject.SetActive(true);
-                        instructMenu.gameObject.SetActive(false);
-                    }
-                    else if (optionMenu.gameObject.activeSelf)
-                    {
-                        //Debug.Log("opt");
-                        mainMenu.gameObject.SetActive(true);
-                        optionMenu.gameObject.SetActive(false);
-                    }
-                }
+	            if (lookTimer > timer) //if length is at the threshold length
+	            {
+	                //Debug.Log("Time "+lookTimer);
+	                //lookTimer = 0f; //reset the lookTimer
+	                myCollider.enabled = false;
+	                /*************************************************************
+	                 * depending on which action is to be taken, the proper
+	                 * function will be called
+	                 * **********************************************************/
+					if (startScene == true) { //switch scenes
+						loading.gameObject.SetActive (true);
+						optionMenu.gameObject.SetActive (false);
+						freeRoamOptionMenu.gameObject.SetActive (false);
+						isLoading = true;
+						StartCoroutine (LoadSceneAsync ());
+					} else if (showOptions == true) { //switch to options menu
+						//Debug.Log("options");
+						optionMenu.gameObject.SetActive (true);
+						mainMenu.gameObject.SetActive (false);
+					} else if (showInstruct == true) { //show instructions menu
+						//Debug.Log("instructions");
+						instructMenu.gameObject.SetActive (true);
+						mainMenu.gameObject.SetActive (false);
+					} else if (showFreeRoamOptions == true) //show free roam difficulty menu
+					{
+						freeRoamOptionMenu.gameObject.SetActive (true);
+						optionMenu.gameObject.SetActive (false);
+					}
+	                else if (goBack == true) //return to previous menu
+	                {
+	                    //Debug.Log("back");
+	                    if (instructMenu.gameObject.activeSelf)
+	                    {
+	                       // Debug.Log("inst");
+	                        mainMenu.gameObject.SetActive(true);
+	                        instructMenu.gameObject.SetActive(false);
+	                    }
+	                    else if (optionMenu.gameObject.activeSelf)
+	                    {
+	                        //Debug.Log("opt");
+	                        mainMenu.gameObject.SetActive(true);
+	                        optionMenu.gameObject.SetActive(false);
+	                    }
+	                }
 
-                resetVars();
-            }
-            
-        }
-        else //iff user stops gaze before threshold time is reached
-        {
-           // Debug.Log("Stop gaze");
-            resetVars();
-        }
+	                resetVars();
+	            }
+	            
+	        }
+	        else //iff user stops gaze before threshold time is reached
+	        {
+	           // Debug.Log("Stop gaze");
+	            resetVars();
+	        }
 
-    }
+	    }
 
-    public void StartGaze() // called once the user sets their reticle on an interactible object
-    {
-        isLookedAt = true;
-    }
+	    public void StartGaze() // called once the user sets their reticle on an interactible object
+	    {
+	        isLookedAt = true;
+	    }
 
-    public void StopGaze()
-    {
-        isLookedAt = false;
-    }
+	    public void StopGaze()
+	    {
+	        isLookedAt = false;
+	    }
 
-    private void resetVars()
-    {
-        if (!isLoading)
-        {
-            lookTimer = 0f;
-            myRenderer.material.SetFloat("cutoff", 0f);
-            startScene = false;
-            showInstruct = false;
-            goBack = false;
-            showOptions = false;
-            scene = "MainMenu";
-            myCollider.enabled = false;
-        }
-    }
-   
-    public void optionsMenu(bool pick)
-    {
-        showOptions = pick;
-    }
+	    private void resetVars()
+	    {
+	        if (!isLoading)
+	        {
+	            lookTimer = 0f;
+	            myRenderer.material.SetFloat("cutoff", 0f);
+	            startScene = false;
+	            showInstruct = false;
+	            goBack = false;
+	            showOptions = false;
+	            scene = "MainMenu";
+	            myCollider.enabled = false;
+	        }
+	    }
+	   
+	    public void optionsMenu(bool pick)
+	    {
+	        showOptions = pick;
+	    }
 
-    public void setScene(string name)
-    {
-        scene = name;
-        startScene = true;
-    }
+	    public void setScene(string name)
+	    {
+	        scene = name;
+	        startScene = true;
+	    }
 
-    public void InstructMenu(bool pick)
-    {
-        showInstruct = pick;
-    }
+	    public void InstructMenu(bool pick)
+	    {
+	        showInstruct = pick;
+	    }
 
-    public void backMenu(bool pick)
-    {
-        goBack = pick;
-    }
+		public void FreeRoamMenu(bool pick){
+			showFreeRoamOptions = pick;
+		}
+	    
+		public void backMenu(bool pick)
+	    {
+	        goBack = pick;
+	    }
+		public void setFreeRoamDifficulty(string difficulty){
+			GameObject maze = GameObject.Find("/Assets/ResourcesEmptyStart");
+			//MazeGenerate mazeSize = maze.GetComponent<MazeGenerate> ();
+			MazeGenerate mazeSize = maze.GetComponent <MazeGenerate>();
+			if (difficulty == "Medium"){
+				Debug.Log (mazeSize.height);
 
-    /// <summary>
-    /// Coroutine which loads a scene asynchronously
-    /// </summary>
-    IEnumerator LoadSceneAsync()
-    {
-        yield return new WaitForSeconds(0.5f);
-        Application.backgroundLoadingPriority = ThreadPriority.Low;
-        Debug.Log("Scene: " + scene);
-        AsyncOperation loadTask = SceneManager.LoadSceneAsync(scene);
-        loadTask.allowSceneActivation = false;
-        yield return loadTask.isDone;
-        yield return new WaitForSeconds(1.0f);
-        SwitchScene(loadTask);
-    }
+			}
+			scene = "FreeRoam";
+			startScene = true;
+		}
+
+	    /// <summary>
+	    /// Coroutine which loads a scene asynchronously
+	    /// </summary>
+	    IEnumerator LoadSceneAsync()
+	    {
+	        yield return new WaitForSeconds(0.5f);
+	        Application.backgroundLoadingPriority = ThreadPriority.Low;
+	        Debug.Log("Scene: " + scene);
+	        AsyncOperation loadTask = SceneManager.LoadSceneAsync(scene);
+	        loadTask.allowSceneActivation = false;
+	        yield return loadTask.isDone;
+	        yield return new WaitForSeconds(1.0f);
+	        SwitchScene(loadTask);
+	    }
 
 
-    private void SwitchScene(AsyncOperation loadTask)
-    {
-        if (loadTask != null)
-        {
-            loadTask.allowSceneActivation = true;
-        }
-    }
+	    private void SwitchScene(AsyncOperation loadTask)
+	    {
+	        if (loadTask != null)
+	        {
+	            loadTask.allowSceneActivation = true;
+	        }
+	    }
+	}
 }
